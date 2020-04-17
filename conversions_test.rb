8dfb2946e97b07f49e8b00dd678e24ea06e3719a
Valeria Johnson <valeria.johnson16@gmail.com>
2020-04-17 15:00:33 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,8 +1,17 @@
 require_relative './conversions.rb'
-puts "ounces_to_grams when given 0, returns 0.0"
+
+puts "Given 0, it should return 0.0"
 answer = ounces_to_grams(0)
 if answer == 0.0
-  puts "Passed the test!"
+  puts "Passed!"
 else
-  puts "Failed the test: got #{answer} instead"
+  puts "Test failed: got #{answer} instead"
+end
+
+puts "Given 1, it should return 28.3495"
+answer = ounces_to_grams(1)
+if answer == 28.3495
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
 end
