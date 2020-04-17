a5e03650f9b094cc075116dececed2c234984545
Valeria Johnson <valeria.johnson16@gmail.com>
2020-04-17 14:52:55 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,10 +1,8 @@
-require_relative '../conversions.rb'
-
+require_relative './conversions.rb'
+Puts "Tests are running..."
+ ./spec/conversions_spec.rb:1:in `<top (required)>'
 describe "conversions" do
   describe '#ounces_to_grams' do
-    it 'given zero, returns 0.0' do
-      grams = ounces_to_grams(0)
-      expect(grams).to eq(0.0)
-    end
+ ruby describe "conversions" do describe '#ounces_to_grams' do it 'given zero, returns 0.0' end end
   end
 end
